8e5633a75aefee841615288895753a13f8b2d767
Youssef <annabiyoussef1986@gmail.com>
2021-02-15 17:26:28 +0100

    repair

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user
      #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url = 'http://monsite.fr/login'
+    @url = 'http://mailertest99.herokuapp.com/login'
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject:'bienvenue chez nous !')
   end
